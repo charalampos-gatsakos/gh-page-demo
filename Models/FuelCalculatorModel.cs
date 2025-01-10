85f7092c5f11e3e88d08f175f27e2d1ffca2475e
Charalampos Gatsakos <93670872+charalampos-gatsakos@users.noreply.github.com>
2025-01-10 14:19:40 +0200

    add fuel calculator functionality + buttons

---
+++ Models/FuelCalculatorModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TotaLift.Client.Models
+{
+    public class FuelCalculatorModel
+    {
+
+        public double? Liters { get; set; } 
+        public double? Distance { get; set; } 
+        public double? TotalCost { get; set; } 
+        public double? FuelConsumptionPer100Km { get; set; } 
+        public double? CostPer100Km { get; set; } 
+
+    }
+}
